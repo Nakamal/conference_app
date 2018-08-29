a3e7075b10814808e5a0f8745ee720da53f8cbc0
Nakamal <nickhaugland@gmail.com>
2018-08-29 15:38:22 -0500

    conferece app hmwk completed and working plus bonus

--- app/controllers/api/speakers_controller.rb
+++ app/controllers/api/speakers_controller.rb
@@ -1,2 +1,6 @@
 class Api::SpeakersController < ApplicationController
+  def one_speaker_action
+    @speaker = Speaker.find_by(first_name: "Toby")
+    render "one_speaker_view.json.jbuilder"
+  end
 end
