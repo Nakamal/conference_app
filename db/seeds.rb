0b1ddbfe5e21c64e0bf328a2f14df371d1623637
Nakamal <nickhaugland@gmail.com>
2018-08-29 15:06:40 -0500

    conference app first commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Speaker.create({first_name: "Dani", last_name: "Zaghian", email: "danielle@actualize.co"})
+Speaker.create({first_name: "Wendy", last_name: "Peffercorn", email: "wendy@actualize.co"})
+Speaker.create({first_name: "Sam", last_name: "Seaborn", email: "sams@thewhitehouse.gov"})
+Speaker.create({first_name: "Toby", last_name: "Ziegler", email: "tobyz@thewhitehouse.gov"})
