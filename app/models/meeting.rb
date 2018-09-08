a5468fe0e900daae3fb3f00b37649bd1c37b30a4
Nakamal <nickhaugland@gmail.com>
2018-09-07 21:09:36 -0500

    added validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,5 @@
 class Meeting < ApplicationRecord
+  validates :time, presence: true, length: { minimum: 5 }
+  validates :location, presence: true
+  validates :agenda, length: { minimum: 2 }
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,10 @@
 class Speaker < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, uniqueness: true
+  validates :age, presence: true, numericality: { greater_than: 17}
 end
+
+
+
+
